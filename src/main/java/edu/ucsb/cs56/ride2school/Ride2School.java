69292eaae2c6d626c7307354603733231c6ab985
Dylan Kupsh <dkupshm@gmail.com>
2018-09-07 09:35:39 -0700

    fixes

--- src/main/java/edu/ucsb/cs56/ride2school/Ride2School.java
+++ src/main/java/edu/ucsb/cs56/ride2school/Ride2School.java
@@ -1,17 +1,15 @@
 package edu.ucsb.cs56.ride2school;
 
+import static spark.Spark.get;
 import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.HashMap;
 
 import org.apache.log4j.Logger;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
-
-import static spark.Spark.get;
-import static spark.Spark.post;
 
 /**
  * Simple example of using Mustache Templates
@@ -20,32 +18,34 @@ import static spark.Spark.post;
 
 public class Ride2School {
 
-    public static final String CLASSNAME =  new Object() {}.getClass().getEnclosingClass().getName();;
+	public static final String CLASSNAME = new Object() {
+	}.getClass().getEnclosingClass().getName();;
 
-    public static final Logger log = Logger.getLogger(CLASSNAME);
+	public static final Logger log = Logger.getLogger(CLASSNAME);
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        port(getHerokuAssignedPort());
+		port(getHerokuAssignedPort());
 
-        Map map = new HashMap();
-        map.put("name", "Sam");
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("name", "Sam");
 
-        // hello.mustache file is in resources/templates directory
-        get("/", (rq, rs) -> new ModelAndView(map, "feed.mustache"), new MustacheTemplateEngine());
+		// hello.mustache file is in resources/templates directory
+		get("/", (rq, rs) -> new ModelAndView(map, "feed.mustache"), new MustacheTemplateEngine());
 
-        get("/form/post", (rq, rs) -> new ModelAndView(map, "post.mustache"), new MustacheTemplateEngine());
+		get("/form/post", (rq, rs) -> new ModelAndView(map, "post.mustache"), new MustacheTemplateEngine());
 
-        post("/login", (rq, rs) -> new ModelAndView(map, "login.mustache"), new MustacheTemplateEngine());
+		post("/login", (rq, rs) -> new ModelAndView(map, "login.mustache"), new MustacheTemplateEngine());
 
-    }
+	}
 
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
-    }
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567; // return default port if heroku-port isn't set (i.e. on
+						// localhost)
+	}
 
 }
